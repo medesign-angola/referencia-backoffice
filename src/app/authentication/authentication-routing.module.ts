76044f3c55e2d6651c4517addb11da6854062d93
IsaquiasMarques <patisaquias2000@gmail.com>
2021-10-18 16:21:51 +0100

    Customize Auth route

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -5,9 +5,10 @@ import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
-  { path: '', component: AuthenticationComponent },
+  // { path: '', component: AuthenticationComponent },
   { path: 'login', component: SignInComponent },
-  { path: 'register', component: SignUpComponent }
+  { path: 'register', component: SignUpComponent },
+  { path: '', redirectTo: 'login', pathMatch: 'full' }
 ];
 
 @NgModule({
