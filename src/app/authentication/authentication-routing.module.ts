5d42a58eb36397ec6955bbe3a13b928993b2f701
IsaquiasMarques <patisaquias2000@gmail.com>
2021-10-21 16:33:40 +0100

    Email verification and Password
    Email verification and Password recovery forms created

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,13 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { EmailFormComponent } from './email-form/email-form.component';
+import { EmailVerificationComponent } from './email-verification/email-verification.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
 
 const routes: Routes = [
   // { path: '', component: AuthenticationComponent },
   { path: 'login', component: SignInComponent },
-  { path: 'register', component: SignUpComponent },
+  { path: 'recover', component: EmailFormComponent },
+  { path: 'verification', component: EmailVerificationComponent }
+  // { path: 'register', component: SignUpComponent },
 ];
 
 @NgModule({
