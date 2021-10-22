a9d4f307cff444c21290d1084e7342951b5297c1
IsaquiasMarques <patisaquias2000@gmail.com>
2021-10-22 14:28:52 +0100

    Authentication forms
    Authentication forms completed

--- src/app/authentication/authentication-routing.module.ts
+++ src/app/authentication/authentication-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthenticationComponent } from './authentication.component';
+import { CodeFormComponent } from './code-form/code-form.component';
 import { EmailFormComponent } from './email-form/email-form.component';
 import { EmailVerificationComponent } from './email-verification/email-verification.component';
 import { SignInComponent } from './sign-in/sign-in.component';
@@ -10,7 +11,8 @@ const routes: Routes = [
   // { path: '', component: AuthenticationComponent },
   { path: 'login', component: SignInComponent },
   { path: 'recover', component: EmailFormComponent },
-  { path: 'verification', component: EmailVerificationComponent }
+  { path: 'verification', component: EmailVerificationComponent },
+  { path: 'code', component: CodeFormComponent }
   // { path: 'register', component: SignUpComponent },
 ];
 
