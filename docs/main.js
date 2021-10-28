f5449415004ad83c085b9a209ebcf35a638eca3f
IsaquiasMarques <patisaquias2000@gmail.com>
2021-10-28 19:49:18 +0100

    Pull docs

--- docs/main.js
+++ docs/main.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\BAXTTER CORPORATION\Pictures\referencia-1\src\main.ts */"zUnb");
+module.exports = __webpack_require__(/*! C:\Users\Pc\Documents\Referência\admin\admin\src\main.ts */"zUnb");
 
 
 /***/ }),
--- docs/polyfills.js
+++ docs/polyfills.js
@@ -7,7 +7,7 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\BAXTTER CORPORATION\Pictures\referencia-1\src\polyfills.ts */"hN/g");
+module.exports = __webpack_require__(/*! C:\Users\Pc\Documents\Referência\admin\admin\src\polyfills.ts */"hN/g");
 
 
 /***/ }),
