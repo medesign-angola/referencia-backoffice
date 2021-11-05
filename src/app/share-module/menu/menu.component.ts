6cca0a822b453f11d106c15b4ea4e3cab7aa3deb
IsaquiasMarques <patisaquias2000@gmail.com>
2021-11-05 22:01:18 +0100

    Page Title improved

--- src/app/share-module/menu/menu.component.ts
+++ src/app/share-module/menu/menu.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Event, NavigationEnd } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-menu',
@@ -7,9 +9,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MenuComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router, 
+    private acti: ActivatedRoute,
+    private loc: Location
+    ) { }
+
+  title: any = this.check();
+  url: string;
 
   ngOnInit(): void {
+    //  console.log(this.router);
+    // console.log(this.acti.snapshot.url.join(''));
+    // console.log(this.router.rou());
+    this.check();
+  }
+
+  check(){
+    this.router.events.subscribe((event: Event) => {
+      if( event instanceof NavigationEnd ){
+        this.url = (<NavigationEnd>event).url;
+        // console.log(this.title);
+        if( this.url === '/admin/imoveis'){
+          this.title = "Imóveis";
+        }else if( this.url === '/admin/proprietarios' ){
+          this.title = "Proprietários";
+        }else if( this.url === '/admin/dashboard' ){
+          this.title = "Dashboard";
+        }else if( this.url === '/admin/solicitacoes' ){
+          this.title = "Solicitações";
+        }else if( this.url === '/admin/usuarios' ){
+          this.title = "Usuários";
+        }else if( this.url === '/admin/definicoes' ){
+          this.title = "Definições";
+        }
+      }
+    })
   }
 
 }
